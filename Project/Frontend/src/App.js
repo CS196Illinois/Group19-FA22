94707089bcb73cff6bfef102da068e3787fbedad
Oliver Rogalski <33262512+oliverrogalski1001@users.noreply.github.com>
2022-10-09 23:34:07 -0500

    Initial React Framework Commit

---
+++ Project/Frontend/src/App.js
@@ -0,0 +1,11 @@
+import { VStack, Heading } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <VStack>
+      <Heading>Stock Sentiment Dashboard</Heading>
+    </VStack>
+  );
+}
+
+export default App;
