ee7720340de00c7e9e64fd349ac3205b621fc1aa
zacharyzhang04 <zachary.z.zhang04@gmail.com>
2022-10-23 13:04:24 -0500

    Add test cases for midterm presentation

--- Project/Backend/lexicon_based_model.py
+++ Project/Backend/lexicon_based_model.py
@@ -24,10 +24,10 @@ import pandas as pd
 
 
 
-# Lexicon-based model
 
 # Paragraphs to words using nltk
-raw = ["AAP SPY no more QE for 2013. Now bring the Geithner"]
+raw = ["The stock market looks really great right now", "Apple stock price went up 6%",
+       "Every stock price is dropping right now", "Looking like a good stock to short"]
 paragraphs = []
 for i in raw:
     fixed = contractions.fix(i)
@@ -93,47 +93,49 @@ for para in cleaned_words:
             temp.append(word)
     important_words.append(temp)
 
-# adding Part of Speech
-currNum = 0
-currSentence = cleaned_words[currNum]
-after_tagging = nltk.pos_tag(currSentence)
+print("\n\n\n")
+for currNum in range(len(raw)):
+    print("INPUT SENTENCE: ", raw[currNum])
+    currSentence = cleaned_words[currNum]
+    after_tagging = nltk.pos_tag(currSentence)
 
-# calculating pos and neg sentiment scores
-p = 0
-n = 0
-score = 0
-for word in currSentence:
-    if word in b:
-        score += b[word]
-for pair in after_tagging:
-    tag = pair[1]
-    word = pair[0]
-    if tag.startswith('J'):
-        synsets = swn.senti_synsets(word, wn.ADJ)
-    elif tag.startswith('V'):
-        synsets = swn.senti_synsets(word, wn.VERB)
-    elif tag.startswith('N'):
-        synsets = swn.senti_synsets(word, wn.NOUN)
-    elif tag.startswith('R'):
-        synsets = swn.senti_synsets(word, wn.ADV)
-    else:
-        synsets = swn.senti_synsets(word, '')
-    if synsets != None:
-        synsets = list(synsets)
-        if len(synsets) > 0:
-            synset = synsets[0]
-            p += synset.pos_score()
-            n += synset.neg_score()
-print("Financial Dictionary Data", score)
-print("Common English Dictionary Model:", (p - n))
+    # calculating pos and neg sentiment scores
+    p = 0
+    n = 0
+    score = 0
+    for word in currSentence:
+        if word in b:
+            score += b[word]
+    for pair in after_tagging:
+        tag = pair[1]
+        word = pair[0]
+        if tag.startswith('J'):
+            synsets = swn.senti_synsets(word, wn.ADJ)
+        elif tag.startswith('V'):
+            synsets = swn.senti_synsets(word, wn.VERB)
+        elif tag.startswith('N'):
+            synsets = swn.senti_synsets(word, wn.NOUN)
+        elif tag.startswith('R'):
+            synsets = swn.senti_synsets(word, wn.ADV)
+        else:
+            synsets = swn.senti_synsets(word, '')
+        if synsets != None:
+            synsets = list(synsets)
+            if len(synsets) > 0:
+                synset = synsets[0]
+                p += synset.pos_score()
+                n += synset.neg_score()
+    print("Financial Dictionary Model", score)
+    print("Common English Dictionary Model:", (p - n))
+    print("\n")
 
-# # Remove stems (lemmatization)
-# port = PorterStemmer()
-# stemmed_words = [port.stem(word) for word in important_words]
-# wordnet = WordNetLemmatizer()
-# lemmatized = [wordnet.lemmatize(word) for word in important_words]
+    # # Remove stems (lemmatization)
+    # port = PorterStemmer()
+    # stemmed_words = [port.stem(word) for word in important_words]
+    # wordnet = WordNetLemmatizer()
+    # lemmatized = [wordnet.lemmatize(word) for word in important_words]
 
 
-## TO DO
-# try textblob and vader
-# add stock words
+    ## TO DO
+    # try textblob and vader
+    # add stock words
