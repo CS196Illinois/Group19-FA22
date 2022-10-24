45ef8b3ef967d62fc122c259097aa28d0d6371d2
Oliver Rogalski <33262512+oliverrogalski1001@users.noreply.github.com>
2022-10-24 16:53:10 -0500

    added comments

--- Project/Backend/main.py
+++ Project/Backend/main.py
@@ -18,17 +18,16 @@ app.add_middleware(
 )
 
 
-# test request returns hello world
-@app.get("/")
+@app.get("/")  # test request returns hello world
 def root():
     return {"message": "Hello World"}
 
 
-@app.get("/api/twitter/sentiment/{ticker}")
+@app.get("/api/twitter/sentiment/{ticker}")  # return list of tweet objects about ticker with time, text, and sentiment
 def get_twitter_sentiment(ticker: str):
     return get_sentiment_data(ticker)
 
 
-@app.get("/api/twitter/volume/{ticker}")
+@app.get("/api/twitter/volume/{ticker}")  # return list of volume for ticker 24 hour timeframes
 def get_twitter_volume(ticker: str):
     return get_volume_data(ticker)
--- Project/Backend/social_media_functions/tweets.py
+++ Project/Backend/social_media_functions/tweets.py
@@ -4,27 +4,33 @@ from Project.Backend.config import settings
 
 
 def get_volume_data(ticker: str) -> List[dict]:
+    # initialize twitter v2 api
     client = tweepy.Client(settings.BEARER_TOKEN)
+
+    # retrieve volume data about ticker in 24 hour timeframes from the past week
     volume = client.get_recent_tweets_count(f"{ticker.upper()}", granularity="day").data
-    return volume
+    return volume[1:]
 
 
 def get_sentiment_data(ticker: str) -> List[dict]:
 
+    # initialize twitter v1 api
     auth = tweepy.OAuth2BearerHandler(settings.BEARER_TOKEN)
     api = tweepy.API(auth)
 
-    data = []
+    data = []   # get all the most popular tweets about a ticker
     for status in tweepy.Cursor(api.search_tweets,
-                                f"${ticker}", count=100,
+                                f"${ticker}",
+                                count=100,
                                 lang="en",
                                 result_type="popular",
                                 tweet_mode="extended").items(300):
 
+        # put the data into a dict
         d = {"time": status.created_at,
              "text": status.full_text,
-             "sentiment": random.random() * 2 - 1}
+             "sentiment": random.random() * 2 - 1}  # placeholder sentiment function
 
         data.append(d)
 
-    return data
+    return data  # return list of tweet dicts
